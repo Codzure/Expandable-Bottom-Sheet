8ade46125f1abb32aabd87887495139f6d34b1d7
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-22 00:31:18 +0300

    Initial Commit

---
+++ BottomSheet/BottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetApp.swift
+//  BottomSheet
+//
+//  Created by Leonard Mutugi on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
